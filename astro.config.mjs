01d7bb0c9377389bd6533ea8f378d8fd14385607
ronald-santos10 <ronalddesign10@gmail.com>
2025-09-02 09:35:37 -0300

    fix SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
-import astroSEO from 'astro-seo-plugin';
 
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://midquest.com.br",
   integrations: [
-    astroSEO(),
     tailwind(),
     partytown({
       config: {
